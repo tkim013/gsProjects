0eca234aa3ad101b9da3b614492a79606cc13cc7
tkim013 <tkim013@gmail.com>
2022-03-23 19:24:37 -0400

    testing

---
+++ GuessNumber/src/test/java/GuessTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuessTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
