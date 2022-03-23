6fdc62cef35471d4f5b992d872704b3b2676afcc
hiddenNPC <102111948+hiddenNPC@users.noreply.github.com>
2022-03-23 19:04:53 -0400

    testing

--- DragonCave2/src/main/java/DragonCave.java
+++ DragonCave2/src/main/java/DragonCave.java
@@ -10,19 +10,27 @@ public class DragonCave {
                 "and will share his treasure with you.  The other dragon \n" +
                 "is greedy and hungry and will eat you on sight.\n" +
                 "Which cave will you go into?  (1 or 2)\n");
-        choice = scan.nextInt();
+        try {
+            choice = scan.nextInt();
 
-        switch (choice) {
-            case 1:
-                System.out.println("You approach the cave...\n" +
-                        "It is dark and spooky...\n" +
-                        "A large dragon jumps out in front of you!  He opens his jaws and...\n" +
-                        "Gobbles you down in one bite!\n");
-                break;
-            case 2:
-                System.out.println("Everything is cool\n" +
-                        "yep");
-                break;
+            switch (choice) {
+                case 1:
+                    System.out.println("You approach the cave...\n" +
+                            "It is dark and spooky...\n" +
+                            "A large dragon jumps out in front of you!  He opens his jaws and...\n" +
+                            "Gobbles you down in one bite!\n");
+                    break;
+                case 2:
+                    System.out.println("You approach the cave...\n" +
+                            "The entrance seems inviting enough as you cautiously peak inside...\n" +
+                            "Inside the cave you find glittering gold and sparkling treasures!\n" +
+                            "A golden nimbus of light appears suddenly and coalesces into the form of a giant dragon!\n" +
+                            "The dragon generously shares his wealth with you!\n" +
+                            "At dinner time, it gobbles you up because... dragon!\n");
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("Exception");
         }
     }
 }
