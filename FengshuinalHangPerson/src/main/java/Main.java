fe292abb2ca62bf38370bdc7447d215046641fcd
tkim013 <tkim013@gmail.com>
2022-04-07 18:31:40 -0400

    It's a start

---
+++ FengshuinalHangPerson/src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int state = 0;
+
+        //one loop to rule them all
+        while (true) {
+
+            display(state);
+            break;
+        }
+        //high score
+    }
+
+    public static void display(int state) {
+        try {
+
+            //read from display.txt
+            Files.readAllLines(Path.of("src/main/resources/display.txt")).stream()
+                    //filter lines starting with state
+                    .filter(e -> e.startsWith(Integer.toString(state)))
+                    //replace state with '_'
+                    .map(s -> s.replace(Character.forDigit(state, 10), ' '))
+                    //print
+                    .forEach(System.out::println);
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
