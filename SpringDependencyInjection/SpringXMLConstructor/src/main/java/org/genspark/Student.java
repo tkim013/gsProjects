21d7ecbbaa5e1a2c253c8fe495ca7f6a21aeec3c
tkim013 <tkim013@gmail.com>
2022-05-11 18:31:20 -0400

    SpringXMLConstructor
    Spring XML Constructor dependency injection

---
+++ SpringDependencyInjection/SpringXMLConstructor/src/main/java/org/genspark/Student.java
@@ -0,0 +1,28 @@
+package org.genspark;
+
+import java.util.List;
+
+public class Student
+{
+    private int id;
+    private String name;
+    private List<Phone> ph;
+    private Address add;
+
+    public Student(int id, String name, List<Phone> ph, Address add) {
+        this.id = id;
+        this.name = name;
+        this.ph = ph;
+        this.add = add;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", ph=" + ph +
+                ", add=" + add +
+                '}';
+    }
+}
